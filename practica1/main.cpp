b9bf0bbbe373ed6e7773f4935d414beb5af8846f
Andrés <agavinm@gmail.com>
2020-01-06 13:27:49 +0100

    Comienzo siguiente parte

--- practica1/main.cpp
+++ practica1/main.cpp
@@ -12,8 +12,7 @@
 
 using namespace std;
 
-int main() {
-    const string f = "../ficheros/";
+void generarDatosPrueba(const string &f) {
     int numProd = 1000;
     const array<float, 11> p = {0.0f, 0.1f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f, 0.7f, 0.8f, 0.9f, 1.0f}; // probabilidades de que dos productos se hayan comprado juntos
 
@@ -38,5 +37,25 @@ int main() {
         delete(cp);
         r++;
     }
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2 && argc != 3) {
+        cout << "Uso:" << endl;
+        cout << '\t' << "Generar datos de prueba: " << argv[0] << " <directorioDondeGenerar>" << endl;
+        cout << '\t' << "Resolver productos: " << argv[0] << " <ficheroProductos> <ficheroCompradosJuntos>" << endl;
+        return 0;
+    }
+
+    if (argc == 2) {
+        string f(argv[1]);
+        if (f[f.length() - 1] != '/')
+            f += '/';
+        generarDatosPrueba(f);
+        return 0;
+    }
+
+    ConjuntoProductos cp(argv[1], argv[2]);
+
     return 0;
 }
