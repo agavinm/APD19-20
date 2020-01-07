b59056aa18bb5de9f5182c19d435f962983e8e5a
Andrés <agavinm@gmail.com>
2020-01-07 20:37:02 +0100

    Entrega práctica 1

---
+++ practica1/ejecutarX.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+make
+
+./practica1 ficheros/f1_10.txt ficheros/f2_10.txt max
+./practica1 ficheros/f1_100.txt ficheros/f2_100.txt max
+./practica1 ficheros/f1_100.txt ficheros/f2_100.txt 50
+./practica1 ficheros/f1_100.txt ficheros/f2_100.txt 1000
+./practica1 ficheros/f1_1000.txt ficheros/f2_1000_0.1.txt 100
+./practica1 ficheros/f1_1000.txt ficheros/f2_1000_0.1.txt 1000
+./practica1 ficheros/f1_1000.txt ficheros/f2_1000_0.5.txt 100
+./practica1 ficheros/f1_1000.txt ficheros/f2_1000_0.5.txt 1000
+./practica1 ficheros/f1_1000.txt ficheros/f2_1000_0.9.txt 100
+./practica1 ficheros/f1_1000.txt ficheros/f2_1000_0.9.txt 1000
+./practica1 ficheros/f1_1000.txt ficheros/f2_1000_0.0.txt 1000
+./practica1 ficheros/f1_1000.txt ficheros/f2_1000_1.0.txt 100
+./practica1 ficheros/f1_1000.txt ficheros/f2_1000_1.0.txt 1000
+./practica1 ficheros/f1_100.txt ficheros/f2_100_0.6.txt max
